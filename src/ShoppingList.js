5edf685d0f063ad38ac4d03ac8f73346b0ac1e50
aks1521 <32307448+aks1521@users.noreply.github.com>
2019-04-17 11:00:10 +0530

    Add files via upload

---
+++ src/ShoppingList.js
@@ -0,0 +1,15 @@
+class ShoppinfgList extends React.Component
+{
+    render() {
+      return (
+        <div className="shopping-list">
+<h1>Shopping List for {this.props.name} </h1>
+<ul>
+    <li>Instagram</li>
+    <li>whatsapp</li>
+    <li>Orcut</li>
+</ul>            
+        </div>
+      )
+    }
+}
